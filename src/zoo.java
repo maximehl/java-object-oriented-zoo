02c53e8ed48424f9a3634ba9957a02c2b02d86df
maximehl <maximehendrikseliu@students.berkeley.net>
2018-02-15 10:21:03 -0800

    Initial commit

---
+++ src/zoo.java
@@ -0,0 +1,63 @@
+public class zoo {
+    public static void main(String[] args){
+        Tiger tigger = new Tiger("Tigger");
+        tigger.eat("meat");
+        tigger.eat("kibble");
+    }
+
+    /*
+    //not using this code anymore because I now have a Tiger class
+
+    public static void main(String[] args) {
+        zoo z = new zoo();
+        z.sleep("Tigger");
+        z.eat("Tigger", "meat");
+        z.eat("Tigger", "bacon");
+    }
+
+    public void sleep(String name) {
+        System.out.println(name + " sleeps for 8 hours");
+    }
+    String favoriteFood = "bacon";
+    public void eat(String name, String food){
+        System.out.println(name + " eats " + food);
+        if(food.equals(favoriteFood)){
+            System.out.println("YUM!!! " + name + " wants more " + food);
+        }else{
+            sleep(name);
+        }
+    }*/
+}
+
+class Tiger {
+    String name;
+    String favoriteFood;
+
+    public Tiger(String name) {
+        this.name = name;
+        this.favoriteFood = "meat";
+    }
+
+    public void sleep() {
+        System.out.println(this.name + " sleeps for 8 hours");
+    }
+
+    public void eat(String food) {
+        System.out.println(this.name + " eats " + food);
+        if(food == this.favoriteFood){
+            System.out.println("YUM! " + this.name + " wants more " + food);
+        }else{
+            this.sleep();
+        }
+    }
+
+    public static void main(String[] args) {
+        //either make a new instance of a zoo
+        //to call the non-static sleep method....
+        Tiger tigger = new Tiger("Tigger");
+        tigger.eat("meat");
+        tigger.eat("kibble");
+
+    }
+}
+
